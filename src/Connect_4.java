64f57c422c7cdc26a9b0d1f5bdc631cc715f7bac
DenaliM <you@example.com>
2025-03-25 00:20:43 -0400

    Added the win condition method, it only checks for rows and columns need a diagnal option as well, added a new connect 4 table constuctor so it can be adjusted at will (hopefully), added some validation to picking columns and when a column is filled to prevent the game from crashing, ya I think thats all

--- src/Connect_4.java
+++ src/Connect_4.java
@@ -19,15 +19,18 @@ public class Connect_4 {
    
       Scanner input = new Scanner(System.in);
       
+      //Constants
+      final int ROWS = 6;
+      final int COLUMNS = 7;
+      
       //Variables
-      int[] board = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //1 = x, 2 = o, 0 = n/a
-      int[] playerMoves; //1 = player, 2 = computer, 0 = n/a
+      int[] board = new int[ROWS * COLUMNS]; //1 = x, 2 = o, 0 = n/a // 1 = player, 2 = player 2, 0 = n/a
       char choice;
       String gameplay;
       
       String username;
       String username2;
-      
+      drawBoard(board, ROWS, COLUMNS);
       System.out.println("What is your name?");
       username = input.nextLine();
       
@@ -48,7 +51,7 @@ public class Connect_4 {
       System.out.println("What is your friends name?");
       username2 = input.nextLine();
       
-      plays(board, username, username2);
+      plays(board, username, username2, ROWS, COLUMNS);
    }
    
    /* 
@@ -56,25 +59,26 @@ public class Connect_4 {
    * Draws the board with the adjusted positions
    *
    */
-   public static void drawBoard(int[] board) {
-      for(int i = 0; i < 42; i++) {
-         if(i%7 == 0) {
-            System.out.println("\n|---|---|---|---|---|---|---|");
-            System.out.print("|");
-         } else {
-            if(board[i] == 1) {
-               System.out.print(" X |");
-            } else if(board[i] == 2) {
-               System.out.print(" O |");
-            } else if (board[i] == 0) {
-               System.out.print("   |");
+   public static void drawBoard(int[] board, int ROWS, int COLUMNS) {
+      for(int i = 0; i < board.length; i++) {
+         if(i%(board.length/ROWS) == 0) {
+            System.out.print("\n|");
+            for(int e = 0; e < board.length/ROWS; e++) {
+               System.out.print("---|");
             }
+            System.out.print("\n|");
+         }
             
-            if(i%7 == 0) {
-               System.out.println();
-            }
-         }  
+         if(board[i] == 1) {
+            System.out.print(" X |");
+         } else if(board[i] == 2) {
+            System.out.print(" O |");
+         } else if (board[i] == 0) {
+            System.out.print("   |");
+         }
       }
+      
+      System.out.print("\n\n");
    }
    
    /* 
@@ -82,14 +86,17 @@ public class Connect_4 {
    * Players play turn
    *
    */
-   public static void plays(int[] board, String username, String username2) {
+   public static void plays(int[] board, String username, String username2, int ROWS, int COLUMNS) {
       Scanner input = new Scanner(System.in);
       int turn = 2;
       int column;
-      int position = 6;
+      int position = (board.length - 1) - ROWS;
+      
+      drawBoard(board, ROWS, COLUMNS);
       
       do {
-         drawBoard(board);
+         position = (board.length - 1) - ROWS;   
+         
          System.out.println("");
          if(turn%2 == 0) {
             System.out.println("\nIts " + username + " turn!");
@@ -97,24 +104,126 @@ public class Connect_4 {
             System.out.println("\nIts " + username2 + " turn!");
          }
          
-         System.out.println("What column do you want to place your piece?");
-         column = input.nextInt();
+         System.out.println("What column do you want to place your piece (1 - 7)?");
+         column = input.nextInt() - 1;
+         
+         do{
+            if(!(column >= 0 && column <= 6) || board[column] != 0) {
+               System.out.println("Column full or invalid, try a different column.");
+               System.out.println("What column do you want to place your piece (1 - 7)?");
+               column = input.nextInt() - 1;
+            }
+         }while(!(column >= 0 && column <= 6) || board[column] != 0);
                   
          do{
-            if(board[position * column] != 0) {
-               position--;
+            if(board[position + column] != 0) {
+               position -= COLUMNS;
             } else {
                break;
             }
-         }while(board[position * column] != 0);
+         }while(board[position + column] != 0);
          
          if(turn%2 == 0) {
-            board[position * column] = 1;
+            board[position + column] = 1;
             turn++;
          } else {
-            board[position * column] = 2;
+            board[position + column] = 2;
+            turn++;
          }
-      }while(true); //Win condition needs to be here
+         
+         drawBoard(board, ROWS, COLUMNS);
+      }while(winCondition(board, ROWS, COLUMNS, username, username2));
    }
-
+   
+   
+   /*
+   *
+   * The condtion to win the connect 4 game
+   *
+   */
+   public static boolean winCondition(int[] board, int ROWS, int COLUMNS, String player1, String player2) {
+      int position = (board.length - 1) - ROWS;
+      int previousPlayer = 0;
+      int currentPlayer = 0;
+      int inARow = 0;
+      
+      //Column Checker
+      
+      int column = 0;
+      do{
+         if(column >= COLUMNS) {
+            break;
+         }
+      
+         if(board[position + column] != 0) {
+            if(board[position + column] == 1) {
+               previousPlayer = currentPlayer;
+               currentPlayer = 1;
+            } else {
+               previousPlayer = currentPlayer;
+               currentPlayer = 2;
+            }
+                  
+            if(previousPlayer == currentPlayer && currentPlayer == board[position + column]) {
+               inARow++;
+            } else {
+               inARow = 0;
+            }
+            
+            position -= COLUMNS;
+            
+            if(inARow >= 3) {
+               if(currentPlayer == 1) {
+                  System.out.print(player1 + " wins!");
+               } else {
+                  System.out.print(player2 + " wins!");
+               }
+               return false;
+            }
+         } else {
+            position = (board.length - 1) - ROWS;
+            inARow = 0;
+            column++;
+         }
+      }while(true);
+      
+      //Row Checker
+      
+      int currentPosition = 0;
+      inARow = 0;
+      do{
+         if(position + currentPosition > board.length - 1) {
+            break;
+         }
+         
+         if(board[position + currentPosition] == 1) {
+               previousPlayer = currentPlayer;
+               currentPlayer = 1;
+            } else {
+               previousPlayer = currentPlayer;
+               currentPlayer = 2;
+            }
+                  
+            if(previousPlayer == currentPlayer && currentPlayer == board[position + currentPosition]) {
+               inARow++;
+            } else {
+               inARow = 0;
+            }
+            
+            currentPosition++;
+            
+            if(inARow >= 3) {
+               if(currentPlayer == 1) {
+                  System.out.print(player1 + " wins!");
+               } else {
+                  System.out.print(player2 + " wins!");
+               }
+               return false;
+            }
+            
+      }while(true);
+      
+      
+      return true; //whatever the win or fail part
+   }
 }
